674a487ee071f70eb029146230d9c2ef565e5c30
shivakumari <shivakumarikumari2515@gmail.com>
2024-09-26 14:58:29 +0530

    Add files via upload

---
+++ program5.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 14:35:20 2024
+
+@author: K.Shiva Kumari
+"""
+
+def dog_age_in_human_years(N):
+    return N * 7
+N=int(input())
+print(dog_age_in_human_years(N))
