e74226e0410812bf118bc5b5ba834203217f02d4
shivakumari <shivakumarikumari2515@gmail.com>
2024-09-26 14:59:47 +0530

    Add files via upload

---
+++ program9.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 26 14:39:46 2024
+
+@author: K.Shiva Kumari
+"""
+
+def count_spaces(S):
+    return S.count(' ')
+
+# Example usage
+S =input()
+space_count = count_spaces(S)
+print(space_count)
